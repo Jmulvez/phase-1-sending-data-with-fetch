0b986b2a621c94c778e9114027042ccc77adac8d
John <johnmulvey48@yahoo.com>
2022-01-04 13:18:25 -0500

    git@github.com:Jmulvez/phase-1-sending-data-with-fetch.git

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData() {
+    fetch('http://localhost:3000/users')
+    .then(res => res.json())
+    .then(submitData => submitData.forEach(user => renderUser(user)))
+}
+
+fetch('http://localhost:3000/users'), {
+    method: 'POST'
+    headers: {
+        'content-type: 'application/json',
+        'accept: 'application/json'
+    .then(res => res.json())
+    .then(user => console.log(user))
+    .catch(function(error)) {
+        alert("A fetch to the API was not found");
+        console.log(error.message);
+    }
+}
+
